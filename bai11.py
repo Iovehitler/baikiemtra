b4d81fcd67549e9b4e37845c41e4405dc1a5a93b
Iovehitler <bmsaigon46@gmail.com>
2025-12-01 16:42:20 +0700

    Add files via upload

---
+++ bai11.py
@@ -0,0 +1,19 @@
+def lasohoanhao(n):
+    tong_uoc = 0
+    
+    for i in range(1, n):
+        if n % i == 0:
+            tong_uoc += i
+    
+    if tong_uoc == n:
+        return True
+    else:
+        return False
+
+
+n = int(input("Nhập một số nguyên dương: "))
+
+if lasohoanhao(n):
+    print(f"{n} là số hoàn hảo")
+else:
+    print(f"{n} không phải là số hoàn hảo")
